720a408a905dece9e5849f35ae3b495bc758dc54
ProgramWithKimta <knguyen4268@gmail.com>
2025-06-02 20:32:57 -0500

    fixing main.tsx code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Apod7Day from './pages/Apod7Day';
-import ApodRandom from './pages/ApodRandom';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
 import Login from './pages/Login';
