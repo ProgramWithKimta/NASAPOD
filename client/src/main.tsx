097da5edaeac07065c4935369e9fb9bbe3b47727
ProgramWithKimta <knguyen4268@gmail.com>
2025-06-04 21:57:06 -0500

    creating not found page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import Apod7day from './pages/Apod7Day';
 import ApodRandom from './pages/ApodRandom';
 import Calendar from './pages/Calendar';
 import FavGallery from './pages/FavoritePhotos';
-import NotFound from './pages/SCNotFound';
+import NotFound from './pages/NotFound';
 import ProtectedRoute from './auth/ProtectedRoute';
 import { AuthProvider } from './auth/AuthProvider';
 
